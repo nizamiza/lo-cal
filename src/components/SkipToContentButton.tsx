93750608ade5a3b968d90f465c641d0dd1ddf6c1
Niza Toshpulatov <niza@hey.com>
2024-05-02 12:41:33 +0200

    Add skip to content button (#4)

---
+++ src/components/SkipToContentButton.tsx
@@ -0,0 +1,35 @@
+import { HTMLAttributes } from "react";
+import { cn } from "@/shared/utils";
+
+type SkipToContentButtonProps = HTMLAttributes<HTMLButtonElement>;
+
+export default function SkipToContentButton({
+  className,
+  ...props
+}: SkipToContentButtonProps) {
+  const handleClick = () => {
+    const firstCalendarDay = document.querySelector(
+      ".calendar-day:first-of-type"
+    ) as HTMLElement | null;
+
+    if (firstCalendarDay) {
+      firstCalendarDay.focus();
+    }
+  };
+
+  return (
+    <button
+      className={cn(
+        "btn surface opacity-0 pointer-events-none",
+        "focus-visible:opacity-100 focus-visible:pointer-events-auto",
+        className
+      )}
+      {...props}
+      type="button"
+      title="Skip to the main content"
+      onClick={handleClick}
+    >
+      Skip to content
+    </button>
+  );
+}
--- src/components/TopNavPanel.tsx
+++ src/components/TopNavPanel.tsx
@@ -1,5 +1,6 @@
 import Today from "@/components/Today";
 import TodayButton from "@/components/TodayButton";
+import SkipToContentButton from "@/components/SkipToContentButton";
 import Settings from "@/components/Settings";
 import Search from "@/components/Search";
 import { cn } from "@/shared/utils";
@@ -13,7 +14,19 @@ export default function TopNavPanel() {
         "items-center gap-2 md:gap-4"
       )}
     >
-      <TodayButton className="justify-self-center sm:justify-self-start" />
+      <div className="grid">
+        <SkipToContentButton
+          className={cn(
+            "row-start-1 row-end-2 col-start-1 col-end-2 focus-visible:order-1"
+          )}
+        />
+        <TodayButton
+          className={cn(
+            "row-start-1 row-end-2 col-start-1 col-end-2",
+            "justify-self-center sm:justify-self-start"
+          )}
+        />
+      </div>
       <Search className="hidden md:inline-flex" />
       <Today />
       <Settings
